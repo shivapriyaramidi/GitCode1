e50ccb2b8d4abf94af1d6ecb2c2293dc6c168e25
priya <priya.siva11@gmail.com>
2021-03-09 17:17:00 -0600

    java

---
+++ JavaPracticeTest/src/main/java/JavaPracticeTest/JavaPracticeTest/FirstJava.java
@@ -0,0 +1,30 @@
+package JavaPracticeTest.JavaPracticeTest;
+
+import java.util.Scanner;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+			
+				// TODO Auto-generated method stub
+		       Scanner scr = new Scanner(System.in);
+		       System.out.println("Enter value ::");
+		       int num;
+		        num = scr.nextInt();
+		       
+		        if( num%2 == 0) {
+		        	System.out.println("given number is even  "  );
+		        }else
+		       
+		       System.out.println("given number is odd  " );
+		       
+			}
+
+		}
+
+	
+
+
